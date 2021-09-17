59fdf744922b0f312f9e731364d9e65ae9697279
Clay <clay.motupalli@gmail.com>
2021-09-17 08:16:12 +0530

    integrated clipboard listener and route to send data

--- server.ts
+++ server.ts
@@ -1,15 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
+const ClipboardListener = require('clipboard-listener');
 
 dotenv.config()
 const port = process.env.PORT || 3000
 
 const app = express();
+
+let clipboardData: any;
+
+const clipboardListener = new ClipboardListener({
+    timeInterval: 100, // Default to 250
+    immediate: true, // Default is false
+});
+
+const startListening = () => {
+    console.log("Started Listening to Clipboard");
+    clipboardListener.on('change', (value: any) => {
+        clipboardData = value;
+        console.log(value);
+    });
+}
+
 app.get("/", (req, res) => {
     let message = "Hello World";
-    res.status(200).send({message});
+    res.status(200).send({ message, clipboardData });
 });
 
 app.listen(port, () => {
     console.log(`Connected and Listening on Port : ${port}`);
+    startListening()
 });
